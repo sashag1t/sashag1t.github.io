a490d4da3e70827046e94e8b8f82c1e7c3f866d1
sashag1t <nghenxtbe@gmail.com>
2025-08-27 17:19:37 +1000

    w5 update

--- week5/script2.js
+++ week5/script2.js
@@ -15,6 +15,11 @@ function subtract(a, b) {
   console.log("value of f", f);
   return f;
 }
+function multiply(a, b) {
+  let g = a * b;
+  console.log("value of g", g);
+  return g;
+}
 // calling of a function
 let sum = add(a, b);
 let sum2 = add(36, 63);
@@ -25,10 +30,17 @@ console.log("value of sum", sum);
 console.log("value of sum", sum3);
 let balance = subtract(100, 35);
 console.log("value of balance", balance);
+let multi = multiply(a, b);
+console.log("value of multi", multi);
 
 function greet(name) {
   let newName = name.toUpperCase();
-  let msg = "hello " + newName;
+  if (newName === "sasha") {
+    msg = "hello " + newName;
+  } else {
+    msg = "sorry I dont know you";
+  }
+  //   let msg = "hello " + newName;
   console.log(msg);
   return newName;
 }
