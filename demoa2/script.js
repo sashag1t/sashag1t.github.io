cc6208397455dbcf5851602eacc537b7ab11137e
sashag1t <nghenxtbe@gmail.com>
2025-09-23 01:27:45 +1000

    update

--- demoa2/script.js
+++ demoa2/script.js
@@ -1,29 +1,43 @@
-// Play button track 1
+const trackList = [
+    { name: "Hes", link: "demoa2/p-hase_Dry-Down-feat-Ben-Snaath.mp3"},
+    { name: "Dry Down", link: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/p-hase_Dry-Down-feat-Ben-Snaath.mp3"},
+    {name: "Leapt", link: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/p-hase_Leapt.mp3"},
+    {name: "Water Feature", link: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/p-hase_Water-Feature.mp3"}
+];
+
+const myAudio = document.querySelector("#my-audio");
+
+// // Play button track 1
 const playButton1 = document.querySelector("#play-button-1");
 console.log(playButton1);
-const Audio1 = document.querySelector("#track1");
-console.log(Audio1);
-playButton1.addEventListener("click", playAudio1());
-function playAudio1(){
-    Audio1.play();
-}
+playButton1.addEventListener("click", function chooseAudio() {
+    playAudio(0);
+});
 
 // Play button track 2
 const playButton2 = document.querySelector("#play-button-2");
 console.log(playButton2);
-const Audio2 = document.querySelector("#track2");
-console.log(Audio2);
-playButton2.addEventListener("click", playAudio2());
-function playAudio2(){
-    Audio2.play();
-}
+playButton2.addEventListener("click", function chooseAudio() {
+    playAudio(1);
+});
 
 // Play button track 3
 const playButton3 = document.querySelector("#play-button-3");
 console.log(playButton3);
-const Audio3 = document.querySelector("#track3");
-console.log(Audio3);
-playButton3.addEventListener("click", playAudio3());
-function playAudio3(){
-    Audio3.play();
+playButton3.addEventListener("click", function chooseAudio() {
+    playAudio(2);
+});
+
+// Play button track 4
+const playButton4 = document.querySelector("#play-button-4");
+console.log(playButton4);
+playButton4.addEventListener("click", function chooseAudio() {
+    playAudio(3);
+});
+
+function playAudio(no) {
+    myAudio.src = trackList.src;
+    console.log(myAudio.src);
+    myAudio.load();
+    myAudio.play();
 }
