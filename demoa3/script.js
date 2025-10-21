1b116de90e88399bc2a3ee295a28c83c8ed45213
sashag1t <nghenxtbe@gmail.com>
2025-10-21 14:39:35 +1100

    FINAL

--- demoa3/script.js
+++ demoa3/script.js
@@ -1,3 +1,22 @@
+const videoWrap = document.querySelector(".video-wrap");
+console.log(videoWrap);
 
+const myVideo = document.querySelector("#video");
+console.log(myVideo);
 
+myVideo.pause();
 
+function scroll() {
+    const distance = window.scrollY - videoWrap.offsetTop;
+    const total = videoWrap.clientHeight - window.innerHeight;
+
+    let percentage = distance / total;
+    percentage = Math.max(0, percentage);
+    percentage = Math.min(percentage, 1);
+
+    if( myVideo.duration > 0) {
+        myVideo.currentTime = myVideo.duration * percentage;
+    }
+} 
+
+window.addEventListener("scroll", scroll);
